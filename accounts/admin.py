dde87fc6743045aa23f65c1dd408f4d58f3e5f44
Pilates Gravity <PilatesGravity@srv1462771.hstgr.cloud>
2026-03-12 21:05:56 +0000

    accounts: registrar UserProfileInline en admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = "Perfil del usuario"
+    fields = (
+        'telefono',
+        'fecha_nacimiento',
+        'sede_preferida',
+        'nivel_experiencia',
+        'acepta_recordatorios',
+        'acepta_marketing',
+        'tiene_lesiones',
+        'descripcion_lesiones',
+        'notas_admin',
+        'puede_ver_pagos',
+    )
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
